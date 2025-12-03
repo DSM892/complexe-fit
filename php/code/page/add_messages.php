01b85d56808eed48693a98aa77b1ecc597e241af
Dayssem SASSI <sassidayssem@gmail.com>
2025-12-03 09:29:10 +0100

    :sparkle: messagerie améliorée

--- php/code/page/add_messages.php
+++ php/code/page/add_messages.php
@@ -2,19 +2,17 @@
 session_start();
 include('bdd.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $content = $_POST['content'];
-
     $sender_id = $_SESSION['id'];
+    $receiver_id = $_POST['other_user_id'];
     $created_at = date('Y-m-d H:i:s');
 
-    $request = $mysqli->prepare("INSERT INTO `MESSAGES` VALUES (NULL, ?, ?, ?);");
-    $request->bind_param("iss", $sender_id, $content, $created_at);
+    $request = $mysqli->prepare("INSERT INTO `MESSAGES` (sender_id, receiver_id, content, created_at) VALUES (?, ?, ?, ?)");
+    $request->bind_param("iiss", $sender_id, $receiver_id, $content, $created_at);
+    $request->execute();
 
-    if ($request->execute()) {
-        header('Location: messagerie.php');
-        exit;
-    }
-    $request->close();
+    header('Location: conversation.php?other_user_id='.$receiver_id);
+    exit;
 }
 ?>
